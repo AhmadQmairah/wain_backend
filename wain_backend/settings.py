ea8966f33e0145410347429ca1d83d75513ec4fc
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-11-06 15:30:32 +0300

    pas

--- wain_backend/settings.py
+++ wain_backend/settings.py
@@ -132,11 +132,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # # Extra lookup directories for collectstatic to find static files
 # STATICFILES_DIRS = (
